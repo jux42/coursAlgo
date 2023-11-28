8ccd2c9f94b9c4b7efe22630a7681f5b9a00dbd4
tom-Violence <roujux@gmail.com>
2023-11-28 13:12:27 +0100

    ""

---
+++ script_php_5.php
@@ -0,0 +1,31 @@
+<?php
+
+$tab[] = null;
+$value = null;
+
+while($value !='q')
+{
+    printf("saisir une valeur numérique ou tapez 'q' pour quitter : ");
+    $value = readline();
+    array_push($tab, $value );
+
+    echo "nombre de valeurs inscrites : " . count($tab) . "\n";
+    echo "somme des valeurs : " . array_sum($tab) . "\n";
+
+    echo "liste des valeurs du tableau : \n";
+    foreach ($tab as $element)
+    {
+        echo $element . "\n";
+    }
+
+sort($tab);
+
+    echo "liste des valeurs triées du tableau : \n";
+    foreach ($tab as $element)
+    {
+        echo $element . "\n";
+    }
+
+}
+
+
